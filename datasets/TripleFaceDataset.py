5f8f3330ca88b297cceecb5787861b5159b52e20
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-09 04:58:26 -0400

    update dataset to choose 1 triplet

--- datasets/TripleFaceDataset.py
+++ datasets/TripleFaceDataset.py
@@ -16,17 +16,20 @@ class TripleFaceDataset(Dataset):
         for faceid in ids:
            files = os.listdir(os.path.join(img_path, faceid))
            for i in range(len(files)):
-                num_triplets = min(5, len(files))
+                num_triplets = min(1, len(files))
 
                 # randomly sample num_triplets
                 for _ in range(num_triplets):
                     img_path1 = os.path.join(img_path, faceid, files[i]) 
-                    img_path2 = os.path.join(img_path, faceid, files[random.randint(0, len(files) - 1)])
+                    img_path2 = img_path1
+                    while img_path2 == img_path1:
+                        img_path2 = os.path.join(img_path, faceid, files[random.randint(0, len(files) - 1)])
 
                     # pick a random image from another face
                     random_faceid = faceid
                     while random_faceid == faceid:
                         random_faceid = ids[random.randint(0, len(ids) - 1)]
+                        
                     random_files = os.listdir(os.path.join(img_path, random_faceid))
                     img_path3 = os.path.join(img_path, random_faceid, random_files[random.randint(0, len(random_files) - 1)])
 
